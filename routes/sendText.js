b2fc5015d93fc859679c546f60a8ba9904f61170
windows channel <info@windowschannel.com>
2022-08-08 22:42:09 -0500

    try return logs

--- routes/sendText.js
+++ routes/sendText.js
@@ -6,10 +6,12 @@ router.post('/', async (req,res)=>{
     // const {auth,numbersToNotificate,message } = req.body    
     // const logs = await service.sendMsgText(auth,numbersToNotificate,message[0].message)
     const {auth, message} = req.body
-    const response = await service.sendTemplate(auth,message)
-    res.status(201).json({
-        response
-    })
+    try {
+        service.sendTemplate(auth,message).then((request)=> console.log(request))
+        
+    } catch (error) {
+        res.json(error)
+    }
 })
 
 module.exports = router
--- services/sendText.js
+++ services/sendText.js
@@ -2,47 +2,39 @@ const axios = require('axios')
 const template = require('../templates-wa/wa_hello_word')
 const graph = 'https://graph.facebook.com/v13.0/101157906035627/messages'
 
-
+function returnLogs(logs){
+    return logs
+}
 async function sendTemplate(auth,message){
     let logs = []
     try {
-        message.numbersToNotificate.forEach(async element => {
+       message.numbersToNotificate.forEach(async element => {
             const templateHelloWorld = template(element.phone)
-            const request = await axios.post(graph,templateHelloWorld, {
-                headers: {
-                  Authorization: `Bearer ${auth.wa_accessToken}`,
-                },
+
+            return new Promise((resolve,reject)=>{
+                axios.post(graph,templateHelloWorld, {
+                    headers: {
+                      Authorization: `Bearer ${auth.wa_accessToken}`,
+                    },
+                }).then((request)=>{
+                    logs.push({
+                        status:request.status,
+                        phone: element.phone,
+                        date: new Date()
+                    })
+                    resolve({
+                        return logs;
+                    })
+
+                }).catch(e => console.error(e))
             })
-            logs.push(request.status)
-            console.log(request.status);
         });
-        return logs
     } catch (error) {
-        console.log(error);
+        console.log(error.code);
+        return error
     }
 
 }
-
-
-
-
-// try {
-//     for (let i = 0; i < numbersToNotificate.length; i++) {
-//         console.log(`---------------${numbersToNotificate[i].phone}-----------------`);
-//         const templateHelloWord = template(phoneToNotificate[i].phone,'hello_world')
-//         const request = await axios.post(graph,templateHelloWord, {
-//             headers: {
-//               Authorization: `Bearer ${auth[1].wa_accessToken}`,
-//             },
-//         })
-//         logs.push(request.status)
-//         console.log(logs);
-//     }
-//     return logs
-// } catch (error) {
-//     console.error(error.code);
-// }
-
 
 module.exports = {
     sendTemplate
